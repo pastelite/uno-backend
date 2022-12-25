a1c4ac5df4fe794ebfa3df77b88299b9af81b254
pastelite <kana.bhong@gmail.com>
2022-12-25 16:43:56 +0700

    finished changing it to number

--- controller/gameController.ts
+++ controller/gameController.ts
@@ -70,7 +70,7 @@ let lobbyStart: RequestHandler = async (req, res, next) => {
 }
 
 let playCard: RequestHandler = async (req, res, next) => {
-  let cardsPlayedString = req.body.card
+  let cardsPlayedNumbers: number[] = req.body.cards
   let cardsListLobby = unoStringToArray(req.lobby!.cardsList)
   let cardsListPlayer = unoStringToArray(req.player!.cardsList)
   let ruleset = rulesetReader(req.lobby!.ruleset)
@@ -82,32 +82,29 @@ let playCard: RequestHandler = async (req, res, next) => {
     return next()
   }
 
-  // check did user have cards
-  if (!cardsPlayedString) {
-    req.message = "where tf is your card"
+  if (!cardsPlayedNumbers) {
+    req.message = "please list card(s) that you want to play in \"cards\""
     return next()
   }
 
-  // check cardlist
-  if (cardsPlayedString.length % 2 != 0 || cardsPlayedString.length == 0) {
-    req.message = "card input incorrect"
-    return next()
-  }
-
-  let cardsPlayed = unoStringToArray(cardsPlayedString)
-  // check if card exist in player hand
-  for (let c of cardsPlayed) {
-    if (!cardsListPlayer.includes(c)) {
-      req.message = "this card not exists in your hand"
+  let cardsPlayed = []
+  for (let i of cardsPlayedNumbers) {
+    let card = cardsListPlayer[i]
+    if (!card) {
+      req.message = "one of the played card is not exists"
       return next()
     }
+    cardsPlayed.push(card)
   }
+  // console.log(cardsListPlayer)
+  // console.log(cardsPlayed)
 
-  // Check color and number of cards
+
+  // find color/number that player played
   let colorPlayed = "?", numberPlayed = "?"
-  let colorTop: string, numberTop: string
+  let colorTop = "?", numberTop = "?"
 
-  // for cardsPlayed
+  // for player
   if (cardsPlayed.length == 1) {
     colorPlayed = cardsPlayed[0][0]
     numberPlayed = cardsPlayed[0][1]
@@ -118,7 +115,7 @@ let playCard: RequestHandler = async (req, res, next) => {
       next()
     }
 
-    // check first cards
+    // check first two cards
     if (cardsPlayed[0][0] == cardsPlayed[1][0]) {
       mode = "color"
     } else if (cardsPlayed[0][1] == cardsPlayed[1][1]) {
@@ -154,8 +151,7 @@ let playCard: RequestHandler = async (req, res, next) => {
   colorTop = cardsListLobby[0][0]
   numberTop = cardsListLobby[0][1]
 
-  // Gameplay
-  // check color/number/symbol
+  // GAMEPLAY
   if (colorTop != colorPlayed && numberTop != numberPlayed && colorPlayed != "S") {
     req.message = "bro, it's not match wtf are you doing"
     return next()
@@ -169,9 +165,107 @@ let playCard: RequestHandler = async (req, res, next) => {
         cardsListLobby,
         numberTop == "T"?2:numberTop == "F"?4:0
       )
+    } else if (numberPlayed) {
+      // TODO: +2/+4
+      // TODO: stack support
     }
-    
   }
+
+  // AFTER ENTIRE THING FINISHED
+  // await updateLobbyCards(req.lobby!.code,cardsListLobby.join(""))
+  // await updatePlayerCards(req.player!.id,cardsListPlayer.join(""))
+
+
+  // // check did user have cards
+  // if (!cardsPlayedString) {
+  //   req.message = "where tf is your card"
+  //   return next()
+  // }
+
+  // // check cardlist
+  // if (cardsPlayedString.length % 2 != 0 || cardsPlayedString.length == 0) {
+  //   req.message = "card input incorrect"
+  //   return next()
+  // }
+
+  // let cardsPlayed = unoStringToArray(cardsPlayedString)
+  // // check if card exist in player hand
+  // for (let c of cardsPlayed) {
+  //   if (!cardsListPlayer.includes(c)) {
+  //     req.message = "this card not exists in your hand"
+  //     return next()
+  //   }
+  // }
+
+  // // Check color and number of cards
+  // let colorPlayed = "?", numberPlayed = "?"
+  // let colorTop: string, numberTop: string
+
+  // // for cardsPlayed
+  // if (cardsPlayed.length == 1) {
+  //   colorPlayed = cardsPlayed[0][0]
+  //   numberPlayed = cardsPlayed[0][1]
+  // } else {
+  //   let mode: "color" | "number"
+  //   let returnCardInput = () => {
+  //     req.message = "card input is not match"
+  //     next()
+  //   }
+
+  //   // check first cards
+  //   if (cardsPlayed[0][0] == cardsPlayed[1][0]) {
+  //     mode = "color"
+  //   } else if (cardsPlayed[0][1] == cardsPlayed[1][1]) {
+  //     mode = "number"
+  //   } else {
+  //     return returnCardInput()
+  //   }
+
+  //   if (mode == "color") {
+  //     colorPlayed = cardsPlayed[0][0]
+  //   } else if (mode == "number") {
+  //     colorPlayed = cardsPlayed[0][1]
+  //   }
+
+  //   // check the rest of cards
+  //   for (let i = 2; i < cardsPlayed.length; i++) {
+  //     switch (mode) {
+  //       case "number":
+  //         if (numberPlayed != numberPlayed[i][0]) {
+  //           return returnCardInput()
+  //         }
+  //         break;
+  //       case "color":
+  //         if (colorPlayed != cardsPlayed[i][0]) {
+  //           return returnCardInput()
+  //         }
+  //         break;
+  //     }
+  //   }
+  // }
+
+  // // for card on the top of lobbyList
+  // colorTop = cardsListLobby[0][0]
+  // numberTop = cardsListLobby[0][1]
+
+  // // Gameplay
+  // // check color/number/symbol
+  // if (colorTop != colorPlayed && numberTop != numberPlayed && colorPlayed != "S") {
+  //   req.message = "bro, it's not match wtf are you doing"
+  //   return next()
+  // }
+
+  // // check for +
+  // if (numberTop == "T" || numberTop == "F") {
+  //   if (ruleset.canStackPlus == false) {
+  //     let cardsDrawed
+  //     [cardsDrawed, cardsListLobby] = drawCardsArray(
+  //       cardsListLobby,
+  //       numberTop == "T"?2:numberTop == "F"?4:0
+  //     )
+  //   }
+    
+  // }
 
   // // logic
   // // normal color case
