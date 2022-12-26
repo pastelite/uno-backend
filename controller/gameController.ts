357834ace5ad1814b45e5eaed8f2447e4bad2cea
pastelite <kana.bhong@gmail.com>
2022-12-26 15:04:36 +0700

    add gameUpdater / cardOnTop fix finding better way than "lobbyUpdater.lobby.playerTurn"

--- controller/gameController.ts
+++ controller/gameController.ts
@@ -3,8 +3,8 @@ const prisma = new PrismaClient()
 
 import { RequestHandler } from "express";
 import rulesetReader from "../utils/rulesetTools";
-import { drawCards, drawCardsArray, unoStringToArray } from "../utils/cardsTools";
-import { updateLobbyCards, updatePlayerCards } from "../utils/gameTools";
+import { LobbyUpdater, PlayerUpdater } from "../utils/gameTools";
+// import { updateLobbyCards, updatePlayerCards } from "../utils/gameTools";
 
 export let getLobbyAction: RequestHandler = async (req, res, next) => {
   switch (req.query.action || req.query.a) {
@@ -26,20 +26,17 @@ export let postLobbyAction: RequestHandler = async (req, res, next) => {
     default:
       req.message = "action is not supported"
       return next()
-    // return res.status(400).json({
-    //   message: "action is not supported"
-    // })
   }
 }
 
 export let lobbyDefaultReply: RequestHandler = async (req, res, next) => {
   // can't be null because of jwtAuthentication
-  let { cardsList, playerList, ...lobby } = req.lobby!
-  let cardOnTop = cardsList.substring(0, 2)
+  let { discardPile, playerList, ...lobby } = req.lobby!
+  let cardOnTop = discardPile.slice(-2)
 
   // count card nums and remove cardsList
   playerList.map((p: any) => {
-    p.cardsCount = p.cardsList.length / 2
+    p.cardsCount = p.cardsOnHand.length / 2
     delete p.cardsList
   })
 
@@ -52,18 +49,24 @@ export let lobbyDefaultReply: RequestHandler = async (req, res, next) => {
 }
 
 let lobbyStart: RequestHandler = async (req, res, next) => {
-  let { code, isStart, cardsList, ...lobby } = req.lobby!
-  let ruleset = rulesetReader(lobby.ruleset)
+  let lobbyU = new LobbyUpdater(req.lobby!)
+  let ruleset = rulesetReader(lobbyU.lobby.ruleset)
 
   // all active player draw
-  for (let player of lobby.playerList) {
-    let cardsOnHand;
-    [cardsOnHand, cardsList] = drawCards(cardsList, ruleset.startDraw)
+  for (let player of req.lobby!.playerList) {
+    let playerU = new PlayerUpdater(player)
+    let drawedCards = lobbyU.draws(ruleset.startDraw)
 
-    await updatePlayerCards(player.id, cardsOnHand)
+    playerU.addCardOnHand(drawedCards)
+
+    await playerU.saveUpdate()
   }
 
-  await updateLobbyCards(code, cardsList)
+  // draw one more card and add to discardpile
+  let drawedCard = lobbyU.draws(1)
+  lobbyU.addDiscardPile(drawedCard)
+
+  await lobbyU.saveUpdate()
 
   req.message = "started"
   next()
@@ -71,14 +74,14 @@ let lobbyStart: RequestHandler = async (req, res, next) => {
 
 let playCard: RequestHandler = async (req, res, next) => {
   let cardsPlayedNumbers: number[] = req.body.cards
-  let cardsListLobby = unoStringToArray(req.lobby!.cardsList)
-  let cardsListPlayer = unoStringToArray(req.player!.cardsList)
-  let ruleset = rulesetReader(req.lobby!.ruleset)
-  let plusCount = req.lobby!.plusCount
-  // let cardOnTop = cardsList[0]
+
+  let lobbyUpdater = new LobbyUpdater(req.lobby!)
+  let playerUpdater = new PlayerUpdater(req.player!)
+
+  let ruleset = lobbyUpdater.getRuleset()
 
   // check turn
-  if (req.lobby!.playerTurn != req.player!.lobbyOrder) {
+  if (lobbyUpdater.lobby.playerTurn != playerUpdater.player.lobbyOrder) {
     req.message = "not your turn"
     return next()
   }
@@ -90,42 +93,34 @@ let playCard: RequestHandler = async (req, res, next) => {
 
   let cardsPlayed = []
   for (let i of cardsPlayedNumbers) {
-    let card = cardsListPlayer[i]
+    let card = playerUpdater.cardsOnHand[i]
     if (!card) {
       req.message = "one of the played card is not exists"
       return next()
     }
     cardsPlayed.push(card)
   }
-  // console.log(cardsListPlayer)
-  // console.log(cardsPlayed)
 
   // check if more than the rule allow
   if (cardsPlayed.length > 1 && ruleset.canStackCards == false) {
+    console.log(ruleset)
     req.message = "cannot stack as specify in the ruleset"
     return next()
   }
 
+  // SET COLOR/NUMBER OF PLAYED CARD
+  let playedColor = cardsPlayed[0][0], playedNumber = cardsPlayed[0][1]
+  let topColor = lobbyUpdater.getTopCard()[0], topNumber = lobbyUpdater.getTopCard()[1]
 
-  // find color/number that player played
-  let colorPlayed = "?", numberPlayed = "?"
-  let colorTop = "?", numberTop = "?"
-
-  // for player
-  if (cardsPlayed.length == 1) {
-    colorPlayed = cardsPlayed[0][0]
-    numberPlayed = cardsPlayed[0][1]
-  } else {
+  // check if it's a stack
+  if (cardsPlayed.length > 1) {
     let mode: "color" | "number" | "plus"
     let returnCardInput = () => {
       req.message = "card input is not match"
       next()
     }
 
-    colorPlayed = cardsPlayed[0][0]
-    numberPlayed = cardsPlayed[0][1]
-
-    // check first two cards
+    // first two card
     if (cardsPlayed[0][0] == cardsPlayed[1][0]) {
       mode = "color"
     } else if (cardsPlayed[0][1] == cardsPlayed[1][1]) {
@@ -135,98 +130,73 @@ let playCard: RequestHandler = async (req, res, next) => {
       && (cardsPlayed[1][1] == "T" || cardsPlayed[1][1] == "F")
     ) {
       mode = "plus"
-      numberPlayed = cardsPlayed[0][1] + cardsPlayed[1][1]
+      playedNumber = cardsPlayed[0][1] + cardsPlayed[1][1]
     } else {
       return returnCardInput()
     }
 
-    // check the rest of cards
+    // the rest
     for (let i = 2; i < cardsPlayed.length; i++) {
       switch (mode) {
-        case "number":
-          if (numberPlayed != numberPlayed[i][0]) {
+        case "color":
+          if (playedColor != cardsPlayed[i][0]) {
             return returnCardInput()
           }
           break;
-        case "color":
-          if (colorPlayed != cardsPlayed[i][0]) {
+        case "number":
+          if (playedNumber != cardsPlayed[i][1]) {
             return returnCardInput()
           }
           break;
         case "plus":
-          if (cardsPlayed[i][0] != "T" || cardsPlayed[i][0] != "F") {
+          if (cardsPlayed[i][1] != "T" || cardsPlayed[i][1] != "F") {
             return returnCardInput();
           } else {
-            numberPlayed += cardsPlayed[i][0];
+            playedNumber += cardsPlayed[i][1];
           }
           break;
       }
     }
   }
 
-  console.log(colorPlayed, numberPlayed)
-
-  // for card on the top of lobbyList
-  colorTop = cardsListLobby[0][0]
-  numberTop = cardsListLobby[0][1]
-
   // GAMEPLAY
-  if (colorTop != colorPlayed && numberTop != numberPlayed && colorPlayed != "S") {
+  if (topColor != playedColor && topNumber != playedNumber && playedColor != "S") {
     req.message = "bro, it's not match wtf are you doing"
     return next()
   }
 
   // + on stack
-  if (numberTop == "T" || numberTop == "F") {
+  if (topNumber == "T" || topNumber == "F") {
     // if cannot stack
     if (ruleset.canStackPlus == false) {
       // draw
-      let cardsDrawed
-      [cardsDrawed, cardsListLobby] = drawCardsArray(
-        cardsListLobby,
-        plusCount
-      )
-      cardsListPlayer.concat(cardsDrawed)
-    // if can stack plus and playing +
-    } else if (numberPlayed[0] == "T" || numberPlayed[0] == "F") {
-      for (let char of numberPlayed) {
-        switch (char) {
-          case "F":
-            plusCount += 2
-          case "T":
-            plusCount += 2
-        }
+      let drawedCard = lobbyUpdater.drawsFromPlusCount()
+      playerUpdater.addCardOnHand(drawedCard)
+      // if can stack plus and playing +
+    } else if (playedNumber[0] == "T" || playedNumber[0] == "F") {
+      for (let char of playedNumber) {
+        lobbyUpdater.addPlusCount(char)
       }
-      console.log(plusCount)
-    // if can stack plus and playing other
+      // if can stack plus and playing other
     } else {
       // draw
-      let cardsDrawed
-      [cardsDrawed, cardsListLobby] = drawCardsArray(
-        cardsListLobby,
-        plusCount
-      )
-      cardsListPlayer.concat(cardsDrawed)
+      let drawedCard = lobbyUpdater.drawsFromPlusCount()
+      playerUpdater.addCardOnHand(drawedCard)
     }
   }
 
   // played +
-  if (numberPlayed[0] == "T" || numberPlayed[0] == "F") {
-    for (let char of numberPlayed) {
-      switch (char) {
-        case "F":
-          plusCount += 2
-        case "T":
-          plusCount += 2
-      }
+  if (playedNumber[0] == "T" || playedNumber[0] == "F") {
+    for (let char of playedNumber) {
+      lobbyUpdater.addPlusCount(char)
     }
-    console.log(plusCount)
   }
 
   // TODO: stop/reverse/thing like that
 
   // AFTER ENTIRE THING FINISHED
-  // await updateLobbyCards(req.lobby!.code,cardsListLobby.join(""))
-  // await updatePlayerCards(req.player!.id,cardsListPlayer.join(""))
-
+  playerUpdater.log()
+  lobbyUpdater.log()
+  // await playerUpdater.saveUpdate()
+  // await lobbyUpdater.saveUpdate()
 }
